955991bb72ddae936cb09dfd8a4786bcaac02587
Raghupathi <raghupathi.jan.01@gmail.com>
2023-05-20 18:57:20 +0530

    store API basic setup

---
+++ 04-store-api/starter/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const ProductsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+app.use(express.json())
+
+app.use('/api/v1/products', ProductsRouter)
+
+app.get('/', (req,res) => {
+    res.send('<h1> Store API</h1><a href="/api/v1/products">products list</a>')
+})
+
+// app.use('api/v1/products', ProductsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async(req, res, next) => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`listening at ${port}......!`);
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+start()
