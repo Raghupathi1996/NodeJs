0f704fc987b0d78f9635ac000749e70a100ce975
Raghupathi <raghupathi.jan.01@gmail.com>
2023-05-25 18:04:31 +0530

    Multiple sort displayed with result being processed at whole

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,30 +2,44 @@ const { query } = require('express')
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-    const search = 'ba'
-    const products = await Product.find({
-        name:{ $regex: search, $options: 'i'}
-    })
+    // const search = 'ba'
+    const products = await Product.find({}).sort('-price')
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) => {
     console.log(req.query)
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
     if(featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
 
     if(company) {
-        queryObject.company = company
+        queryObject.company = company  // company attribute extracted
     }
 
     if(name) {
-        queryObject.name = { $regex: name, $options: 'i'}
+        queryObject.name = { $regex: name, $options: 'i'}  // regular expression for the names
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject)
+
+    // let product = Product.find(queryObject)
+    // console.log("after find query");
+    // if(sort) {
+    //     product = await product.sort(sort)
+    //     console.log("sorting");
+    // }
+
+    let result = Product.find(queryObject)
+    if(sort) {
+        const sortList = sort.split(",").join(" ")
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
+
+    // const product = await result
     res.status(200).json({products, nbHits: products.length})
 }
 
